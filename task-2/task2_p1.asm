4673b67e8e49866d4f0d0c476ba207e01d54de18
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2022-01-06 18:18:20 +0200

    Finished Task2 and Task3

--- task-2/task2_p1.asm
+++ task-2/task2_p1.asm
@@ -1,30 +1,34 @@
-;%include "../../io.mac"
 section .text
 	global cmmmc
-	;extern printf
 
-;; int cmmmc(int a, int b)
-;
-;; calculate least common multiple fow 2 numbers, a and b
 cmmmc:
-
-
-	;eax = primul parametru
-	;edx = al doilea parametru
-
+	;Registrul eax stocheaza primul parametru iar registrul 
+	;edx cel de al doilea parametru
+	
+	;Cele doua numere sunt impinse pe stack
 	push eax
 	push edx
+	;Se stocheaza valoarea lui edx in ecx si valoarea lui eax in edi, stiva
+	;devenind in acest moment goala
 	pop ecx
 	pop edi
+	;Cele doua numere sunt din nou impinse pe stiva si se stocheaza apoi in registrul
+	;eax inmultirea dintre acesta si edx cu ajutorul lui imul
 	push eax 
 	push edx
 	imul eax, edx
+	;Se impinge produsul celor doua numere pe stiva si apoi se scoate in registrul ebx
 	push eax 
-	pop ebx ;x * y
+	pop ebx
 
-	pop ecx ;y
-	pop edx ;x
+	;Se salveaza in continuare valoarea celor doua numere in registrele ecx si edx
+	pop ecx
+	pop edx 
 
+	;Se calculeaza cel mai mare divizor comun folosind algoritmul deja cunoscut 
+	;prin scaderi repetate ale numarului mai mic din cel mai mare. In momentul in 
+	;care cel mai mic a ajuns la valoarea 0, cel mai mare divizor comun este constituit
+	;de numarul mare
 	gcd:
 		cmp ecx, dword 0
 		je finish 
@@ -33,7 +37,7 @@ cmmmc:
 		continue:
 			sub edx, ecx
 			jmp gcd 
-
+	;Se interschimba valorile celor doua numere cu ajutorul stivei
 	swap_values:
 		push edx 
 		push ecx 
@@ -41,7 +45,11 @@ cmmmc:
 		pop ecx 
 		jmp continue
 
-
+;La final rezultatul celui mai mare divizor comun este stocat in ecx. Cel mai mic 
+;multiplu comun reprezinta produsul numerelor impartit la cel mai mare divizor comun al acestora,
+;mai exact ebx / edx. Se va stoca valoarea lui ebx in eax si valoarea lui edx in ecx pentru a
+;putea face divizia cu ajutorul lui idiv. In final, rezultatul se afla in eax, registru ce este
+;returnat de catre functie
 finish:
 	push ebx 
 	push dword 1
--- task-2/task2_p2.asm
+++ task-2/task2_p2.asm
@@ -1,56 +1,53 @@
-;%include "../../io.mac"
 section .text
 	global par
-	;extern printf
 
-;; int par(int str_length, char* str)
-;
-; check for balanced brackets in an expression
 par:
-	
-
-	; eax al doilea parametru
-	; edx primul parametru
-	; ( = 40
-	; ) = 41
+	;Registrul edx stocheaza primul parametru (dimensiunea sirului) iar
+	;registrul eax stocheaza cel de al doilea parametru, sirul de caractere
 
 	push 0
 	pop ecx
 	push 0
 	pop edi
 
+	;Se parcurge intreg sirul de caractere si se monitorizeaza cu ajutorul
+	;registrului edi parantezarea la fiecare pas. Conditiile pentru ca 
+	;parantezarea sa fie corecta este ca pe tot parcursul trecerii prin sir, numarul de
+	;paranteze ( sa nu fie mai mic decat numarul de paranteze ). De asemenea, la final trebuie
+	;verificat daca numarul de paranteze ( nu este diferit de 0.
 	traverse:
-		cmp ecx, edx 
+		cmp ecx, edx 	
 		je end_traverse 
-		push 40
-		pop ebx 
-		cmp bl, [eax + ecx]
+		push 40 			; 40 reprezinta codul Ascii pentru caracterul '(', acesta fiind impins pe stack
+		pop ebx 			; Se stocheaza caracterul '(' in subregistrul bl care este comparat cu caracterul 
+		cmp bl, [eax + ecx] ; curent. Daca acestea sunt egale, valoarea lui edi creste, in caz contrar, scade
 		je adding
 		jmp substracting
 		continue:
-			cmp edi, dword 0
-			jl wrong
-			add ecx, dword 1
+			cmp edi, dword 0 ;Se face comparatia intre edi si 0, daca edi este mai mic decat 0, procesul se termina 
+			jl wrong		 ;returnand valoarea 0
+			add ecx, dword 1 ;In caz contrar se continua parcurgerea
 			jmp traverse
-	
+	;La sfarsitul traversarii prin sirul de caractere se verifica daca edi este egal cu 0, daca da, 
+	;parantezarea este corecta
 	end_traverse:
 		cmp edi, dword 0
 		jg wrong
 		jmp right
-
+;Adaugarea unei paranteze '('
 adding:
 	add edi, dword 1
 	jmp continue
-
+;"Adaugarea" unei paranteze ')'
 substracting:
 	sub edi, dword 1
 	jmp continue
-
+;Cazul in care parantezarea este gresita
 wrong:
 	push 0
 	pop eax 
 	jmp finish
-
+;Cazul in care parantezarea este corecta
 right:
 	push 1
 	pop eax 
